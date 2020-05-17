6b92a8913995852eb7fe0fd45b010ef51ad76553
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-17 23:33:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,18 +1,18 @@
 class TicTacToe
 
- def initialize 
+ def initialize
   @board = Array.new(9, " ") #establishes a new board in the form of an array with 9 spaces
  end
 
  WIN_COMBINATIONS = [ # a nested array filled with all the possible ways to form three in a row on the grid
-    [0, 1, 2], 
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6], 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8], 
-    [0, 4, 8], 
-    [6, 4, 2]  
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
   ]
 
  def display_board #  establishes the physical shape of the board and tells it where each space on the board array exists
@@ -23,11 +23,11 @@ class TicTacToe
    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
  end
 
- def input_to_index(player_input) 
+ def input_to_index(player_input)
     player_input.to_i - 1 #because the array starts at 0 instead of 1, this tells the program to subtract 1 from the user's input
  end
 
- def move(index, current_player = "X") 
+ def move(index, current_player = "X")
    @board[index] = current_player
  end
 
@@ -39,11 +39,11 @@ class TicTacToe
    index.between?(0,8) && !position_taken?(index)
  end
 
- def turn 
-   puts "Please choose a number 1-9:" 
+ def turn
+   puts "Please choose a number 1-9:"
    user_input = gets.chomp # recives the user input
    index = input_to_index(user_input) # this will translate tha input int a index value
-   if valid_move?(index) # checks if the move is valid before proceeding 
+   if valid_move?(index) # checks if the move is valid before proceeding
      player_token = current_player
      move(index, player_token) #makes the move on the board
      display_board
@@ -97,9 +97,9 @@ class TicTacToe
 
   def draw? # the game is a draw is the somebody wins or if the board is full
     if !won? && full?
-      return true 
+      return true
     else
-      return false 
+      return false
     end
   end
 
@@ -111,7 +111,7 @@ class TicTacToe
     end
   end
 
-  def winner 
+  def winner
     index = []
     index = won?
     if index == false
