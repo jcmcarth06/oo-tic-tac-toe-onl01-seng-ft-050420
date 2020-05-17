8e49b527f6b567b189ed7d97f1d04d53192214bf
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-17 23:33:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,21 +1,21 @@
 class TicTacToe
 
- def initialize # this is when a new instance of the game is initialized.
-  @board = Array.new(9, " ") # instance variable named board with a new array of 9 empty stings creathing the board.
+ def initialize 
+  @board = Array.new(9, " ") #establishes a new board in the form of an array with 9 spaces
  end
 
- WIN_COMBINATIONS = [ # a nested array filled with the index favlues fo rthe various winning combinations possible.
-    [0, 1, 2], # top horizontal row
-    [3, 4, 5], # middle horizontal row
-    [6, 7, 8], # bottom horizontal row
-    [0, 3, 6], # left vertical row
-    [1, 4, 7], # middle vertical row
-    [2, 5, 8], # right vertical row
-    [0, 4, 8], # diagonal left to right
-    [6, 4, 2]  # diagonal right to left
+ WIN_COMBINATIONS = [ # a nested array filled with all the possible ways to form three in a row on the grid
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [6, 4, 2]  
   ]
 
- def display_board # a method that will print the current board.
+ def display_board #  establishes the physical shape of the board and tells it where each space on the board array exists
    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
    puts "-----------"
    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -23,36 +23,36 @@ class TicTacToe
    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
  end
 
- def input_to_index(player_input) # a method to pass user input
-    player_input.to_i - 1 # indicex in the array start at 0m -1 to correctly translate the players perspective.
+ def input_to_index(player_input) 
+    player_input.to_i - 1 #because the array starts at 0 instead of 1, this tells the program to subtract 1 from the user's input
  end
 
- def move(index, current_player = "X") #must take in two arguments, the index in the board and the players token
+ def move(index, current_player = "X") 
    @board[index] = current_player
  end
 
- def position_taken?(index) # will check index values of the #move method. then check to see if that postion is vacent
+ def position_taken?(index) # a position is available if it is not occupied by another piece or is empty
    !(@board[index].nil? || @board[index] == " ")
  end
 
- def valid_move?(index) # this will check if a move is valid and not already filled with a token
+ def valid_move?(index) # a move is valid if it is on the board and it is false that the position is taken
    index.between?(0,8) && !position_taken?(index)
  end
 
- def turn # a single complete turn
-   puts "Please choose a number 1-9:" #asks the user for their move
+ def turn 
+   puts "Please choose a number 1-9:" 
    user_input = gets.chomp # recives the user input
    index = input_to_index(user_input) # this will translate tha input int a index value
-   if valid_move?(index) # if the move is valid,
+   if valid_move?(index) # checks if the move is valid before proceeding 
      player_token = current_player
-     move(index, player_token) #make the move and display it on the board
+     move(index, player_token) #makes the move on the board
      display_board
    else
-     turn #ask for input again
+     turn #the turn is over, requests a new loop
    end
  end
 
- def turn_count # this method returns the number of turns that have been played based on the @board variable
+ def turn_count # when no turns have been played, the counter is at zero. With each turn, the counter goes up by one and returns the turn number
    turn = 0
    @board.each do |index|
    if index == "X" || index == "O"
@@ -62,7 +62,7 @@ class TicTacToe
   return turn
   end
 
-  def current_player # this will use the #turn_count methon to determine if its a "X" or "O" turn
+  def current_player # if the last player was X, then it is O's turn and vice versa
       num_turns = turn_count
       if num_turns % 2 == 0
         player = "X"
@@ -72,7 +72,7 @@ class TicTacToe
       return player
   end
 
-  def won? # should return false/nil if there is no win combination, or return the winning combination index as an array if there is a win
+  def won? # a win is when a payer gets one of the predetermined three in a rows
     WIN_COMBINATIONS.each {|win_combo|
       index_0 = win_combo[0]
       index_1 = win_combo[1]
@@ -91,19 +91,19 @@ class TicTacToe
     return false
   end
 
-  def full? # method should return true if every element in the board contains either an "X" or an "O"
+  def full? # the board is full if every space is occupied by either an x or an o
     @board.all? {|index| index == "X" || index == "O"}
   end
 
-  def draw?
+  def draw? # the game is a draw is the somebody wins or if the board is full
     if !won? && full?
-      return true # returns true if the board is full and has not been won
+      return true 
     else
-      return false # false if the board is won, and false if the board is neither won nor full
+      return false 
     end
   end
 
-  def over? # returns true if the board has been won or is full
+  def over? # The game is over if there is a win or draw
     if won? || draw?
       return true
     else
@@ -111,11 +111,11 @@ class TicTacToe
     end
   end
 
-  def winner # Given a winning @board
+  def winner 
     index = []
     index = won?
     if index == false
-      return nil # the winning method should return x,o that has won the game
+      return nil #returns the name of the winner
     else
       if @board[index[0]] == "X"
         return "X"
@@ -126,13 +126,13 @@ class TicTacToe
   end
 
   def play
-    until over? == true # until the game is over take turns
+    until over? == true # ugame keeps playing until it is over
       turn
     end
     if won? # if the game was won, congratulate the winner
       puts "Congratulations #{winner}!"
     elsif draw?
-      puts "Cat's Game!" #else if the game was a ddraw, tell the player its end in a draw
+      puts "Cat's Game!" #else if the game was a draw, tell the player its end in a draw
     end
   end
 end
